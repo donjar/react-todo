d549f74bae2acb009bbbddc9061f0702df7c0990
Herbert Ilhan Tanujaya <mail@herbert.id>
2017-04-30 23:26:12 +0800

    life is hard

--- src/App.js
+++ src/App.js
@@ -2,27 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
+class TodoItem extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>To-do List</h1>
-        </div>
-        <TodoList />
-      </div>
-    );
+      <div key={this.props.name} onClick={this.props.handleClick}>{this.props.name}</div>
+    )
   }
 }
 
-function TodoItem(props) {
-  return (
-    <li key={props.name}>{ props.name }</li>
-  )
+class TodoList extends React.Component {
+  renderItem(props) {
+    return (
+      <TodoItem key={props.name} name={props.name} handleClick={props.handleClick(props.name)} />
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.todoItems.map(item => this.renderItem({
+          name: item.name,
+          handleClick: this.props.handleClick,
+        }))}
+      </div>
+    )
+  }
 }
 
-class TodoList extends React.Component {
+class App extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -32,19 +39,25 @@ class TodoList extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="input-group">
-          <input type="text" id="add-todo-input" className="form-control" />
-          <span className="input-group-btn">
-            <button className="btn btn-default" type="button"
-                    onClick={() => this.addTodo(document.getElementById("add-todo-input").value)}>
-              Add Todo
-            </button>
-          </span>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>To-do List</h1>
         </div>
-        <ul className="TodoList">
-          { this.state.todoItems.map(TodoItem) }
-        </ul>
+
+        <div className="container">
+          <div className="input-group">
+            <input type="text" id="add-todo-input" className="form-control" />
+            <span className="input-group-btn">
+              <button className="btn btn-default" type="button"
+                      onClick={() => this.addTodo(document.getElementById("add-todo-input").value)}>
+                Add Todo
+              </button>
+            </span>
+          </div>
+
+          <TodoList todoItems={this.state.todoItems} handleClick={name => this.todoHandleClick(name)} />
+        </div>
       </div>
     )
   }
@@ -58,6 +71,16 @@ class TodoList extends React.Component {
     this.setState({
       todoItems: this.state.todoItems.concat({ name: name }),
     });
+  }
+
+  todoHandleClick(name) {
+    console.log("called");
+    /*
+    const newTodoItems = this.state.todoItems.filter(todoItem => { return todoItem.name !== name });
+    this.setState({
+      todoItems: newTodoItems,
+    })
+    */
   }
 }
 
