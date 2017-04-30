196d7da9fc2d507fbae6bd82f43f82a87b802037
Herbert Ilhan Tanujaya <mail@herbert.id>
2017-04-30 22:23:13 +0800

    Basic create/read todo list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>To-do List</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TodoList />
       </div>
     );
+  }
+}
+
+function TodoItem(props) {
+  return (
+    <li key={props.name}>{ props.name }</li>
+  )
+}
+
+class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todoItems: [],
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="input-group">
+          <input type="text" id="add-todo-input" className="form-control" />
+          <span className="input-group-btn">
+            <button className="btn btn-default" type="button"
+                    onClick={() => this.addTodo(document.getElementById("add-todo-input").value)}>
+              Add Todo
+            </button>
+          </span>
+        </div>
+        <ul className="TodoList">
+          { this.state.todoItems.map(TodoItem) }
+        </ul>
+      </div>
+    )
+  }
+
+  addTodo(name) {
+    console.log(this.state.todoItems);
+    if (this.state.todoItems.some(todoItem => { return todoItem.name === name })) {
+      alert("Cannot have same name");
+      return;
+    }
+
+    this.setState({
+      todoItems: this.state.todoItems.concat({ name: name }),
+    });
   }
 }
 
